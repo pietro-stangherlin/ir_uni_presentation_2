478cee8f47ecb4c33c4cd2bc5d43ba1c9d2477ec
pietro-stangherlin <125908132+pietro-stangherlin@users.noreply.github.com>
2023-04-27 12:27:28 +0200

    updated query and relevances files

--- query_rel_scripts/query.py
+++ query_rel_scripts/query.py
@@ -4,5 +4,10 @@
 
 QUERIES = {
     1:"memory",
-    2:"galaxy"
+    2:"elephants",
+    3:"Hermann Ebbinghaus",
+    4:"chess",
+    5:"climate change",
+    6:"painting art in France",
+    7:"location of the most expensive stones on Earth"
 }
